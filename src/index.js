bd51e9b976d3750b8769db15ad3d973737943052
Alexey V.Ryapisov <tealikes@yandex.ru>
2020-10-11 14:05:15 +0300

    Added styles

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './style.scss'
 import { HashRouter as Router } from 'react-router-dom'
 import Routes from 'routes'
 
 const App = () => {
   return (
     <Router>
-      Электрик СПБ
       <Routes />
     </Router>
   )
